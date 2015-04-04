1ee3a36609bc3c32c7b859f8072f7a3af733a0cf
Denis Lila <denilila@gmail.com>
2015-04-04 16:09:25 -0700

    remove unused function

--- toys/crypto1/w1/cipherBreak.py
+++ toys/crypto1/w1/cipherBreak.py
@@ -13,15 +13,6 @@ messages = [x.decode('hex') for x in ["315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc
 "466d06ece998b7a2fb1d464fed2ced7641ddaa3cc31c9941cf110abbf409ed39598005b3399ccfafb61d0315fca0a314be138a9f32503bedac8067f03adbf3575c3b8edc9ba7f537530541ab0f9f3cd04ff50d66f1d559ba520e89a2cb2a83",
 "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"]]
 
-def count_list_elems(l):
-    result = {}
-    for x in l:
-        if x in result:
-            result[x] += 1
-        else:
-            result[x] = 1
-    return result
-
 def highest_pairs(m):
     """ m is a map with numeric values. Return a list of keys that have the largest value. """
     maxv = max(m.values())
