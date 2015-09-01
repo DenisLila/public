ebb0e2749aa044df7719e7085c053c04735228b7
Denis Lila <denilila@gmail.com>
2015-09-01 14:43:23 -0700

    improve PWGEN

--- toys/java/com/dlila/pwdgen/PwdGen.java
+++ toys/java/com/dlila/pwdgen/PwdGen.java
@@ -1,17 +1,25 @@
-package com.dlila.pwdgen;
-
+import java.util.HashSet;
+import java.util.Set;
 import java.security.SecureRandom;
 
 public class PwdGen {
-
-  private static final int LEN = 10;
+  private static final int LEN = 16; 
   private static final String CHARS;
 
   static {
     String chars = "abcdefghijklmnopqrstuvwxyz";
     chars += chars.toUpperCase();
-    chars += "1234567890!@#$%^&*()=+-+";
+    chars += "1234567890!@#$%^&*()=+-[]{}<>,.`~/?|'";
     CHARS = chars;
+
+    // verify that there are no duplicates in CHARS
+    Set<Character> charSet = new HashSet<>();
+    for (Character c : CHARS.toCharArray()) {
+      charSet.add(c);
+    }   
+    if (charSet.size() != CHARS.length()) {
+      throw new IllegalStateException();
+    }   
   }
 
   public static void main(String[] argv) {
@@ -19,7 +27,7 @@ public class PwdGen {
     StringBuilder b = new StringBuilder(LEN);
     for (int i = 0; i < LEN; i++) {
       b.append(CHARS.charAt(random.nextInt(CHARS.length())));
-    }
+    }   
     System.out.println(b.toString());
   }
 }
