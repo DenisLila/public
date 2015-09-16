1e347e7cf6b9e0ec52e03fa34554bf5b7a620be9
Denis Lila <denilila@gmail.com>
2015-09-15 22:33:30 -0700

    minor improvements to tempreader

--- java/com/dlila/tempserver/TempReader.java
+++ java/com/dlila/tempserver/TempReader.java
@@ -3,12 +3,15 @@ package com.dlila.tempserver;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
+import com.google.common.base.Splitter;
 import com.google.common.io.LineReader;
 
 class TempReader {
 
   private final InputStream is;
+  private final Splitter inputLineSplitter;
   
   // TODO(dlila): how do we export this to the outside world cleanly?
   // (don't forget concurrency).
@@ -17,15 +20,19 @@ class TempReader {
   // TODO(dlila): don't forget to make this buffered.
   public TempReader(InputStream is) {
     this.is = is;
+    this.inputLineSplitter = Splitter.on(',');
   }
 
   public void readTemps() throws IOException {
     LineReader lineReader = new LineReader(new InputStreamReader(is));
     String line = null;
     do {
-      line = lineReader.readLine();
-      String[] split = line.split(',');
-      currentTemp = split[split.length - 1];
+      List<String> split = inputLineSplitter.splitToList(lineReader.readLine());
+      try {
+        currentTemp = Double.parseDouble(split.get(split.size() - 1));
+      } catch (NumberFormatException e) {
+        throw new IOException(e);
+      }
     } while (line != null);
   }
 }
