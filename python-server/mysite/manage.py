7233fdd545e4fc114ead9dd8c757f5f83e540d74
Denis Lila <denilila@gmail.com>
2015-10-04 19:54:54 -0700

    set dontwritebytecode

--- python-server/mysite/manage.py
+++ python-server/mysite/manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    sys.dont_write_bytecode = True
 
     from django.core.management import execute_from_command_line
 
